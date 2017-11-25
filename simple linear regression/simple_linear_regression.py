67bb19434f38614ee3998e24bea7e9ea95d8b7c4
Nahid Shaiket <nahidshaiket10300@gmail.com>
2017-11-25 23:31:24 +0600

    First commit

---
+++ simple linear regression/simple_linear_regression.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 24 23:29:36 2017
+
+@author: Nahid
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 1].values
+
+#Splitting the dataset into training set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=1/2, random_state=0)
+
+#Feature scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
+#fitting simple linear regression to training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+#predicting the test set result
